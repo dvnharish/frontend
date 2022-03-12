d36f73d577ed9c7e370cf773c5262847f212225a
Harish <mail2duddupudi@gmail.com>
2022-03-12 14:48:55 +0530

    Changes for Experience

--- resume-pages/js/main.js
+++ resume-pages/js/main.js
@@ -10,3 +10,21 @@ window.onscroll = function () {
   }
   prevScrollpos = currentScrollPos;
 };
+
+var experiences = document.getElementsByClassName("experiencebutton");
+
+for (let index = 0; index < experiences.length; index++) {
+  experiences[index].addEventListener("click", function () {
+    var content = this.nextElementSibling;
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+      content.style.color = "aliceblue";
+      content.style.borderRadius = "10px";
+      content.style.boxShadow = "0px 10px 10px 0px rgb(4, 19, 41)";
+      content.style.zIndex = "2";
+      content.style.backgroundColor = "rgb(10, 28, 59)";
+    }
+  });
+}
